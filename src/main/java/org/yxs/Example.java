e1b47da6c8c751a02db51ec79cc47e0ba10b62e1
王湛智 <wangzhanzhi@xinnet.com>
2016-05-25 13:53:14 +0800

    修改示例

--- src/main/java/org/yxs/Example.java
+++ src/main/java/org/yxs/Example.java
@@ -45,72 +45,36 @@ public class Example
         @Size(max = 10, min = 2)
         private List<String> list;
 
-        public String getUname() {
-            return uname;
-        }
-
         public void setUname(String uname) {
             this.uname = uname;
-        }
-
-        public String getPasswd() {
-            return passwd;
         }
 
         public void setPasswd(String passwd) {
             this.passwd = passwd;
         }
 
-        public String getEmail() {
-            return email;
-        }
-
         public void setEmail(String email) {
             this.email = email;
-        }
-
-        public String getEmpty() {
-            return empty;
         }
 
         public void setEmpty(String empty) {
             this.empty = empty;
         }
 
-        public String getBlank() {
-            return blank;
-        }
-
         public void setBlank(String blank) {
             this.blank = blank;
-        }
-
-        public String getNumber() {
-            return number;
         }
 
         public void setNumber(String number) {
             this.number = number;
         }
 
-        public String getPattern() {
-            return pattern;
-        }
-
         public void setPattern(String pattern) {
             this.pattern = pattern;
         }
 
-        public String getLength() {
-            return length;
-        }
-
         public void setLength(String length) {
             this.length = length;
-        }
-
-        public List<String> getList() {
-            return list;
         }
 
         public void setList(List<String> list) {
@@ -126,14 +90,15 @@ public class Example
         user.setBlank("");
         user.setNumber("1");
         user.setPattern("12");
+        user.setLength("a");
 
-        System.out.println();
         Validator validator = Validator.newInstance(user);
+
         long start = new Date().getTime();
         Set<Medusa> set = validator.result();
         for (Medusa medusa : set) {
             System.out.println(medusa.toString());
         }
-        System.out.println(new Date().getTime() - start);
+        System.out.println("消耗 " + (new Date().getTime() - start) + " MS");
     }
 }
